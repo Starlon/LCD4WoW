82c4f41cd5bf278b0558f9d92e310c4a6368ed92
Scott Sibley <sisibley@gmail.com>
2011-06-17 17:46:01 +0000

    Doc test
    git-svn-id: svn+ssh://svn.wowace.com/wow/libscriptable-1-0/mainline/trunk@88 cfb1be40-04d6-4bea-a26a-ed40e111ccc7

--- LibScriptableWidgets-1.0/LibScriptableWidgetBar-1.0/LibScriptableWidgetBar-1.0.lua
+++ LibScriptableWidgets-1.0/LibScriptableWidgetBar-1.0/LibScriptableWidgetBar-1.0.lua
@@ -292,7 +292,7 @@ end
 -- @param db The database table
 -- @param callback Provide this if you want to execute the callback once an option is changed
 -- @param data Some data to pass when executing the callback
--- @return An Ace3 options table -- `name.args = options`.
+-- @return An Ace3 options table: `name.args = options`.
 function WidgetBar:GetOptions(db, callback, data)
 	local defaults = WidgetBar.defaults
 	local options = {
