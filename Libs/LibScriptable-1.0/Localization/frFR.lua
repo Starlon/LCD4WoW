d324c41611333fa8b07531e14a6e5113ff84e14f
Scott Sibley <sisibley@gmail.com>
2011-07-25 20:05:11 -0500

    Remove libraries merged by git-externals.py.

--- Libs/LibScriptable-1.0/Localization/frFR.lua
+++
@@ -1,4 +0,0 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("LibScriptable-1.0", "frFR")
-if not L then return end
-
---@localization(locale="frFR", format="lua_additive_table", handle-subnamespaces="concat", handle-unlocalized="english")@
--- Libs/LibScriptable-1.0/Localization/zhCN.lua
+++
@@ -1,4 +0,0 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("LibScriptable-1.0", "zhCN")
-if not L then return end
-
---@localization(locale="zhCN", format="lua_additive_table", handle-subnamespaces="concat", handle-unlocalized="english")@
